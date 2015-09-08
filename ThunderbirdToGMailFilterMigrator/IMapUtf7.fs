3bb22dc0ee3379e644a326fc2082f43dbdedae40
locatw <loca.gm@gmail.com>
2015-09-08 21:31:29 +0900

    IMAP-UTF7のデコードモジュールを実装

---
+++ ThunderbirdToGMailFilterMigrator/IMapUtf7.fs
@@ -0,0 +1,115 @@
+﻿module IMapUtf7
+
+open System
+open System.Text
+
+type internal GroupType =
+| Printable
+| ModifiedBase64
+    override self.ToString() =
+        match self with
+        | Printable -> "Printable"
+        | ModifiedBase64 -> "Modified Base64"
+
+type internal Group = 
+    {
+        Type : GroupType;
+        Bytes : byte list;
+    }
+    override this.ToString() =
+        sprintf "{Type = %s; Bytes = %A}" (this.Type.ToString()) this.Bytes
+
+/// <summary>
+/// Devide ASCII string into printable and modified Base64 string groups.
+/// </summary>
+/// <param name="input"></param>
+let internal devideToPrintableAndUnprintable (input : string) =
+    let isAmpersand (b : byte) = b = 0x26uy
+
+    let isHyphen (b : byte) = b = 0x2duy
+
+    let rec devide (bytes : byte list) =
+        match bytes with
+        | [] -> []
+        // when start with "&-", take it as printable
+        | first::second::tail when isAmpersand first && isHyphen second ->
+            let newGroup = { Type = Printable; Bytes = [first; second] }
+            newGroup::(devide tail)
+        // when start with "&", take modified Base64 bytes group
+        | head::tail when isAmpersand head ->
+            let hyphenIndex = List.tryFindIndex isHyphen tail
+            match hyphenIndex with
+            | Some(index) ->
+                let left, right = List.splitAt index tail
+                let newGroup = { Type = ModifiedBase64; Bytes = left }
+                // skip '-' from right
+                newGroup::(devide (List.skip 1 right))
+            | None -> failwith "invalid format"
+        // when start with printable byte, take printable bytes group
+        | _ ->
+            let ampersandIndex = List.tryFindIndex isAmpersand bytes
+            match ampersandIndex with
+            | Some(index) ->
+                let left, right = List.splitAt index bytes
+                let newGroup = { Type = Printable; Bytes = left }
+                newGroup::(devide right)
+            | None ->
+                [{ Type = Printable; Bytes = bytes }]
+
+    let rec combineContinuousPrintableGroup (groups : Group list) =
+        match groups with
+        | head::second::tail when head.Type = Printable && second.Type = Printable ->
+            let combinedBytes = List.append head.Bytes second.Bytes
+            let combinedGroup = { Type = Printable; Bytes = combinedBytes }
+            combineContinuousPrintableGroup (combinedGroup::tail)
+        | head::tail ->
+            head::(combineContinuousPrintableGroup tail)
+        | _ ->
+            groups
+
+    Encoding.ASCII.GetBytes(input)
+    |> Array.toList
+    |> devide
+    |> combineContinuousPrintableGroup 
+
+let internal decodeIMapUtf7 (input : string) =
+    let replaceCommaToSlash (s : string) =
+        s.Replace(',', '/')
+
+    let padWith (c : char) (s : string) =
+        let replicate letter (times : int) =
+            new String(letter, times)
+
+        match s.Length % 4 with
+        | 0 -> s
+        | n when 1 <= n && n <= 3 -> s + (replicate c (4 - n))
+        | _ -> failwith "never occurs"
+
+    let decodeBase64 (s : string) =
+        Encoding.BigEndianUnicode.GetString(Convert.FromBase64String(s))
+
+    input
+    |> replaceCommaToSlash
+    |> padWith '='
+    |> decodeBase64
+
+let private decodeModifiedBase64Group (group : Group) =
+    let bytes = List.toArray group.Bytes
+    match group.Type with
+    | Printable -> Encoding.ASCII.GetString(bytes)
+    | ModifiedBase64 -> decodeIMapUtf7 (Encoding.ASCII.GetString(bytes))
+
+/// <summary>
+/// Decode IMAP-UTF7 string.
+/// </summary>
+/// <params name="input">Ascii string</params>
+/// <returns>decoded UTF-16 string</returns>
+let decode (input : string) =
+    let replaceAmpersandHyphenToAmpersand (s: string) =
+        s.Replace("&-", "&")
+
+    input
+    |> devideToPrintableAndUnprintable
+    |> List.map decodeModifiedBase64Group
+    |> String.concat ""
+    |> replaceAmpersandHyphenToAmpersand
---
+++ UnitTest/IMapUtf7Test.fs
@@ -0,0 +1,123 @@
+﻿module IMapUtf7.Test
+
+open NUnit.Framework
+open FsUnit
+open IMapUtf7
+
+type System.String
+with
+    member this.toByteList =
+        Array.toList (System.Text.Encoding.ASCII.GetBytes(this))
+
+[<TestFixture>]
+type ``devideToPrintableAndUnprintable tests`` () =
+    [<Test>]
+    static member ``devide string contains only printable letters to one group`` () =
+        "ab"
+        |> devideToPrintableAndUnprintable
+        |> should equal [{ Type = Printable; Bytes = [(byte)'a'; (byte)'b'] }]
+
+    [<Test>]
+    static member ``devide string contains only unprintable letters to one group`` () =
+        "&ab-"
+        |> devideToPrintableAndUnprintable
+        |> should equal [{ Type = ModifiedBase64; Bytes = [(byte)'a'; (byte)'b'] }]
+
+    [<Test>]
+    static member ``&- treats as printable bytes`` () =
+        "a&-b"
+        |> devideToPrintableAndUnprintable
+        |> should equal [{ Type = Printable; Bytes = [(byte)'a'; (byte)'&'; (byte)'-'; (byte)'b'] }]
+
+    [<Test>]
+    static member ``devide to printable and modified Base64`` () =
+        "a&b-c"
+        |> devideToPrintableAndUnprintable
+        |> should equal [
+            { Type = Printable; Bytes = [(byte)'a'] };
+            { Type = ModifiedBase64; Bytes = [(byte)'b'] };
+            { Type = Printable; Bytes = [(byte)'c'] }
+        ]
+
+    [<Test>]
+    static member ``devide to printable and two modified Base64`` () =
+        "a&b-c&d-e"
+        |> devideToPrintableAndUnprintable
+        |> should equal [
+            { Type = Printable; Bytes = [(byte)'a'] };
+            { Type = ModifiedBase64; Bytes = [(byte)'b'] };
+            { Type = Printable; Bytes = [(byte)'c'] };
+            { Type = ModifiedBase64; Bytes = [(byte)'d'] };
+            { Type = Printable; Bytes = [(byte)'e'] };
+        ]
+
+    [<Test>]
+    static member ``devide when modified Base64 at first`` () =
+        "&a-b"
+        |> devideToPrintableAndUnprintable
+        |> should equal [
+            { Type = ModifiedBase64; Bytes = [(byte)'a'] };
+            { Type = Printable; Bytes = [(byte)'b'] }
+        ]
+
+    [<Test>]
+    static member ``devide when modified Base64 at last`` () =
+        "a&b-"
+        |> devideToPrintableAndUnprintable
+        |> should equal [
+            { Type = Printable; Bytes = [(byte)'a'] };
+            { Type = ModifiedBase64; Bytes = [(byte)'b'] };
+        ]
+
+[<TestFixture>]
+type ``tests for decodeModifiedUtf7 function``() =
+    [<TestCase("ZeVnLIqe", "日本語")>]
+    static member ``decode simple Modeified UTF7 string``(input : string, expected : string) =
+        input
+        |> decodeIMapUtf7
+        |> should equal expected
+
+    // "ＡＡＡ" is not need '=' and contains '/' when encode to Base64,
+    // so it can test decoding string contains comma purely.
+    [<TestCase(",yH,If8h", "ＡＡＡ")>]
+    static member ``decode Modified UTF7 string contains comma``(input : string, expected : string) =
+        input
+        |> decodeIMapUtf7
+        |> should equal expected
+
+    // When "ab" is encoded in Base64, it becomes "AGEAYg==".
+    // But Modified UTF7 does not require padding by '='.
+    [<TestCase("AGEAYg", "ab")>]
+    static member ``decode Modified UTF7 string that length is not multiple of 4``(input : string, expected : string) =
+        input
+        |> decodeIMapUtf7
+        |> should equal expected
+
+[<TestFixture>]
+type ``decode tests`` () =
+    [<TestCase("abc", "abc")>]
+    static member ``decode modified UTF7 contains no modified Base64`` (input : string) (expected : string) =
+        input
+        |> decode
+        |> should equal expected
+
+    [<TestCase("a&-b", "a&b")>]
+    static member ``decode modified UTF7 contains no modified Base64 but contains "&-"``
+            (input : string) (expected : string) =
+        input
+        |> decode
+        |> should equal expected
+
+    [<TestCase("&,yH,If8h-", "ＡＡＡ")>]
+    static member ``decode modified UTF7 contains only modified Base64``
+            (input : string) (expected : string) =
+        input
+        |> decode
+        |> should equal expected
+
+    [<TestCase("a&,yH,If8h-b", "aＡＡＡb")>]
+    static member ``decode modified UTF7 contains normal string and modified Base64``
+            (input : string) (expected : string) =
+        input
+        |> decode
+        |> should equal expected
